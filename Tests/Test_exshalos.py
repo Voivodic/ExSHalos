d8e45e6487dd3a6c02b97b5c32d2403019c8cd28
Voivodic <rodrigo.voivodic@usp.br>
2022-04-20 16:19:03 +0200

    Inclusion of new examples for testing

--- Tests/Test_exshalos.py
+++ Tests/Test_exshalos.py
@@ -3,28 +3,104 @@ import exshalos
 import pylab as pl
 import time
 
+#Parameters
+Lc = 2.0
+Nd = 256
+L = Lc*Nd
+Nk = 64
+Window = "CIC"
+
 #Open the linear power spectrum
 k, P = np.loadtxt("MDPL2_z00_matterpower.dat", unpack = True)
 
 #Compute the correlation function
+print("Computing the correlation")
 R, Xi = exshalos.simulation.Compute_Correlation(k, P, verbose = True)
 k2, P2 = exshalos.simulation.Compute_Correlation(R, Xi, direction = -1, verbose = True)
 
 #Plot the correlation
+pl.clf()
 pl.loglog(k, P, "-")
 pl.loglog(k2, P2, "--")
 pl.savefig("Correlation.pdf")
 
 #Compute the density grid
-grid = exshalos.utils.Generate_Density_Grid(k, P, Lc = 8.0, nd = 256)
+print("Generating the density grid")
+grid = exshalos.utils.Generate_Density_Grid(k, P, Lc = Lc, nd = Nd)
 print(grid.shape)
 
-ks, Ps, Nks = exshalos.simulation.Compute_Power_Spectrum(grid, L = 2048.0, window = 0, Nk = 64, nthreads = 1, l_max = 0)
+ks, Ps, Nks = exshalos.simulation.Compute_Power_Spectrum(grid, L = L, window = 0, Nk = Nk, nthreads = 1, l_max = 0)
 
 print(Ps.shape)
 
+pl.clf()
 pl.loglog(k, P, "-")
-pl.loglog(ks, Ps[0,0,:], "o")
+pl.loglog(ks, Ps, "o")
 pl.ylim(1e+2, 5e+4)
 pl.xlim(5e-3, 1e+0)
 pl.savefig("Power_sim.pdf")
+
+#Find the halos in the grid above
+print("Finding the halos")
+posh, Mh = exshalos.utils.Find_Halos_from_Grid(grid, k, P, Lc = Lc, Nmin = 1)
+print(posh.shape, Mh.shape)
+
+print(np.min(posh), np.max(posh))
+print("%e %e" %(np.min(Mh), np.max(Mh)))
+
+gridh = exshalos.simulation.Compute_Density_Grid(posh, nd = Nd, L = L, window = Window, interlacing = True, nthreads = 1, verbose = False)
+kh, Ph, Nkh = exshalos.simulation.Compute_Power_Spectrum(gridh, L = L, window = Window, Nk = Nk, nthreads = 1, l_max = 0)
+
+pl.clf()
+pl.loglog(kh, Ph , "s")
+pl.loglog(ks, Ps, "o")
+#pl.ylim(1e+2, 5e+4)
+#pl.xlim(5e-3, 1e+0)
+pl.savefig("Power_halos.pdf")
+
+pl.clf()
+pl.hist(Mh, bins = np.logspace(11, 15, 41), log=True)
+pl.xscale("log")
+pl.savefig("Hist_Mh.pdf")
+
+#Compute the LPT using the same grid
+print("Doing LPT")
+
+pl.clf()
+#pl.plot(ks, Ps, "o", label = "Gaussian")
+k_smooth = [0.1, 0.15, 0.2, 0.3, 1.0]
+cores = ["blue", "red", "darkgreen", "purple", "black"]
+for ks, cor in zip(k_smooth, cores):
+    pos = exshalos.utils.Displace_LPT(grid, Lc = Lc, k_smooth = ks, DO_2LPT = False)
+
+    gridp = exshalos.simulation.Compute_Density_Grid(pos, nd = Nd, L = L, window = Window, interlacing = True, nthreads = 1)
+    kp, Pp, Nkp = exshalos.simulation.Compute_Power_Spectrum(gridp, L = L, window = Window, Nk = Nk, nthreads = 1, l_max = 0)
+
+    pl.plot(kp, Pp/Ps - 1.0, "s", color = cor, label = r"$k_{\rm smooth} = %.2f$" %(ks))
+
+    pos = exshalos.utils.Displace_LPT(grid, Lc = Lc, k_smooth = ks, DO_2LPT = True)
+
+    gridp = exshalos.simulation.Compute_Density_Grid(pos, nd = Nd, L = L, window = Window, interlacing = True, nthreads = 1)
+    kp, Pp, Nkp = exshalos.simulation.Compute_Power_Spectrum(gridp, L = L, window = Window, Nk = Nk, nthreads = 1, l_max = 0)
+
+    pl.plot(kp, Pp/Ps - 1.0, "^", color = cor)   
+
+pl.grid(True)
+pl.yscale("linear")
+pl.ylim(-0.5, 0.25)
+pl.xlim(0.0, 0.6)
+pl.legend(loc = "best")
+pl.savefig("Power_LPT.pdf")
+
+print(np.mean(grid), np.std(grid))
+print(np.mean(gridp[0]), np.std(gridp[0]))
+print(np.mean(gridh[0]), np.std(gridh[0]))
+
+pl.clf()
+pl.subplot(131)
+pl.imshow(grid[128,:,:])
+pl.subplot(132)
+pl.imshow(gridp[0,128,:,:])
+pl.subplot(133)
+pl.imshow(gridh[0,128,:,:])
+pl.savefig("Grids.pdf")
--- Tests/Test_spectrum.py
+++ Tests/Test_spectrum.py
@@ -7,6 +7,12 @@ import time
 #Generate the fake data
 L = 4000.0
 Nd = 256
+ntypes = 1
+interlacing = True
+window = "CIC"
+nthreads = 4
+Nk = 5
+verbose = False
 
 #Read the halo catalogue
 f = h5py.File('/home/voivodic/Documents/Multi_EFT/MD4_M13.2_xyz_Vxyz_TF.hdf5','r')  
@@ -17,55 +23,64 @@ f.close()
 print(logM.shape, pos.shape)
 
 types = np.zeros(len(logM))
-types[logM > 13.5] = 1
+if(ntypes == 2):
+    types[logM > 13.5] = 1
+else:
+    types = None
 
 #Compute the density grid
 print("Computing the density grid")
 start = time.time()
-g1 = exshalos.simulation.Compute_Density_Grid(pos, nd = Nd, type = types, L = L, window = "NGP", interlacing = True, nthreads = 1, verbose = False)
+g1 = exshalos.simulation.Compute_Density_Grid(pos, nd = Nd, type = types, L = L, window = window, interlacing = interlacing, nthreads = nthreads, verbose = verbose)
 end = time.time()
 
 print("Time took = %f" %(end - start))
 print(g1.shape)
+print(np.mean(g1[0]), np.std(g1[0]))
 
 #Compute the power spectra
 print("Computing the power spectrum")
 start = time.time()
-k, P, Nk = exshalos.simulation.Compute_Power_Spectrum(g1, L = L, window = "NGP", Nk = 5, nthreads = 1, verbose = False)
+k, P, Nmodes = exshalos.simulation.Compute_Power_Spectrum(g1, L = L, window = window, Nk = Nk, nthreads = nthreads, verbose = verbose, ntype = ntypes)
 end = time.time()
 
 print("Time took = %f" %(end - start))
-print(k.shape, P.shape, Nk.shape)
+print(k.shape, P.shape, Nmodes.shape)
 
 print("Computing the bispectrum")
 start = time.time()
-kP2, P2, Nk2, kB, B, Ntri = exshalos.simulation.Compute_BiSpectrum(g1, L = L, window = "NGP", Nk = 5, nthreads = 4, verbose = False)
+kP2, P2, Nmodes2, kB, B, Ntri = exshalos.simulation.Compute_BiSpectrum(g1, L = L, window = window, Nk = Nk, nthreads = nthreads, verbose = verbose, ntype = ntypes)
 end = time.time()
 
 print("Time took = %f" %(end - start))
-print(kB.shape, B.shape, Ntri.shape, kP2.shape, P2.shape, Nk2.shape)
+print(kB.shape, B.shape, Ntri.shape, kP2.shape, P2.shape, Nmodes2.shape)
 
 print("Computing the trispectrum")
 start = time.time()
-kP3, P3, Nk3, kT, T, Tu, Nsq = exshalos.simulation.Compute_TriSpectrum(g1, L = L, window = "NGP", Nk = 5, nthreads = 4, verbose = False)
+kP3, P3, Nmodes3, kT, T, Tu, Nsq = exshalos.simulation.Compute_TriSpectrum(g1, L = L, window = window, Nk = Nk, nthreads = nthreads, verbose = verbose, ntype = ntypes)
 end = time.time()
 
 print("Time took = %f" %(end - start))
-print(kT.shape, T.shape, Tu.shape, Nsq.shape, kP3.shape, P3.shape, Nk3.shape)
-print(np.log10(T), np.log10(Tu), np.log10(T - Tu))
+print(kT.shape, T.shape, Tu.shape, Nsq.shape, kP3.shape, P3.shape, Nmodes3.shape)
 
-#pl.clf()
-pl.loglog(k, P[0,:], "-", color = "red")
-pl.loglog(k, P[1,:], "-", color = "red")
-pl.loglog(k, P[2,:], "-", color = "red")
+pl.clf()
+if(ntypes == 2):
+    pl.loglog(k, P[0,:], "-", color = "red")
+    pl.loglog(k, P[1,:], "-", color = "red")
+    pl.loglog(k, P[2,:], "-", color = "red")
 
-pl.loglog(kP2, P2[0,:], "--", color = "blue")
-pl.loglog(kP2, P2[1,:], "--", color = "blue")
-pl.loglog(kP2, P2[2,:], "--", color = "blue")
+    pl.loglog(kP2, P2[0,:], "--", color = "blue")
+    pl.loglog(kP2, P2[1,:], "--", color = "blue")
+    pl.loglog(kP2, P2[2,:], "--", color = "blue")
 
-pl.loglog(kP3, P3[0,:], ":", color = "darkgreen")
-pl.loglog(kP3, P3[1,:], ":", color = "darkgreen")
-pl.loglog(kP3, P3[2,:], ":", color = "darkgreen")
+    pl.loglog(kP3, P3[0,:], ":", color = "darkgreen")
+    pl.loglog(kP3, P3[1,:], ":", color = "darkgreen")
+    pl.loglog(kP3, P3[2,:], ":", color = "darkgreen")
+
+else:
+    pl.loglog(k, P, "-", color = "red")
+    pl.loglog(kP2, P2, "--", color = "blue")
+    pl.loglog(kP3, P3, ":", color = "darkgreen")
 
 pl.savefig("Test_power.pdf")
 
