0022a19ada2a9ccccae175ad0b37c172cd1531f9
Voivodic <rodrigo.voivodic@usp.br>
2022-05-19 10:28:16 +0200

    Aditional tests

--- Tests/Fit_barrier.py
+++ Tests/Fit_barrier.py
@@ -24,23 +24,24 @@ dnt = exshalos.theory.dlnndlnm(Mt, sigma = sigma, model = 2, theta = 300, delta_
 fdn = interp1d(Mt, dnt)
 
 #Fit the barrier
-print("Fitting the barrier")
+'''print("Fitting the barrier")
 barrier = None#np.array([0.54296115, 0.3974207,  0.69304439])
-x = exshalos.utils.Fit_Barrier(k, P, Mt, dnt, grid = None, R_max = 100000.0, Mmin = -1.0, Mmax = -1.0, Nm = 25, nd = Nd, Lc = Lc, Om0 = Om0, z = z, delta_c = -1.0, Nmin = 1, seed = 12345, x0 = barrier, verbose = False, nthreads = 1)
+x = exshalos.utils.Fit_Barrier(k, P, Mt, dnt, grid = None, R_max = 100000.0, Mmin = -1.0, Mmax = -1.0, Nm = 25, nd = Nd, Lc = Lc, Om0 = Om0, z = z, delta_c = -1.0, Nmin = 1, seed = 12345, x0 = barrier, verbose = False, nthreads = 1, tol = 0.1)
 
 print(x.x)
 
 #Generate a halo catologue with this barrier
 print("Generating the halo catalogues")
-barrier = np.array(x.x)
+barrier = np.array(x.x)'''
+barrier = [0.67376829, 0.35238359, 0.44854763]
 
 print("First catalogue")
-x = exshalos.mock.Generate_Halos_Box_from_Pk(k, P, nd = Nd, Lc = Lc, Om0 = Om0, z = z, k_smooth = 0.4, Nmin = 1, a = barrier[0], beta = barrier[1], alpha = barrier[2], seed = 12345, OUT_DEN = False, OUT_LPT = False, OUT_VEL = False, DO_2LPT = False, OUT_FLAG = False, verbose = False, nthreads = 1)
-dnh1 = exshalos.simulation.Compute_Abundance(x["Mh"], Lc = Lc, nd = Nd)
+x = exshalos.mock.Generate_Halos_Box_from_Pk(k, P, nd = Nd*2, Lc = Lc, Om0 = Om0, z = z, k_smooth = 0.4, Nmin = 1, a = barrier[0], beta = barrier[1], alpha = barrier[2], seed = 12345, OUT_DEN = False, OUT_LPT = False, OUT_VEL = False, DO_2LPT = False, OUT_FLAG = False, verbose = False, nthreads = 1)
+dnh1 = exshalos.simulation.Compute_Abundance(x["Mh"], Lc = Lc, nd = Nd*2)
 
 print("Second catalogue")
-x = exshalos.mock.Generate_Halos_Box_from_Pk(k, P, nd = Nd, Lc = Lc, Om0 = Om0, z = z, k_smooth = 0.4, Nmin = 1, seed = 12345, OUT_DEN = False, OUT_LPT = False, OUT_VEL = False, DO_2LPT = False, OUT_FLAG = False, verbose = False, nthreads = 1)
-dnh2 = exshalos.simulation.Compute_Abundance(x["Mh"], Lc = Lc, nd = Nd)
+x = exshalos.mock.Generate_Halos_Box_from_Pk(k, P, nd = Nd*2, Lc = Lc, Om0 = Om0, z = z, k_smooth = 0.4, Nmin = 1, seed = 12345, OUT_DEN = False, OUT_LPT = False, OUT_VEL = False, DO_2LPT = False, OUT_FLAG = False, verbose = False, nthreads = 1)
+dnh2 = exshalos.simulation.Compute_Abundance(x["Mh"], Lc = Lc, nd = Nd*2)
 
 #Plot the mass functions
 pl.clf()
