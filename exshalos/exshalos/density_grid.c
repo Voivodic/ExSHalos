85d2592834b44fd50581baf68d7c4700f8af07e9
Voivodic <rodrigo.voivodic@usp.br>
2022-08-22 11:48:53 +0200

    Fixing in the generation of the density grid to properly construct the grid in Fourier space

--- exshalos/exshalos/density_grid.c
+++ exshalos/exshalos/density_grid.c
@@ -49,17 +49,17 @@ void Inter_Power(fft_real *K, fft_real *P, int Nk, fft_real R_max, gsl_spline *s
         P_tmp[i] = (double) P[i];
     }
   
-    /*Compute the Power spectrum in the box*/
+    /*Compute the Power spectrum in the box
     if(R_max < 100000.0){
         pk2xi(Nk, K_tmp, P_tmp, R_xi, Xi);
         for(i=0;i<Nk;i++)
             if(R_xi[i] > R_max)	
                 Xi[i] = 0.0;
         xi2pk(Nk, R_xi, Xi, K_tmp, P_tmp);
-    }
+    }*/
 
 	/*Interpolate the power spectrum*/
-	gsl_spline_init(spline,  K_tmp, P_tmp, Nk);
+	gsl_spline_init(spline, K_tmp, P_tmp, Nk);
 
 	free(R_xi);
 	free(Xi);
@@ -99,55 +99,92 @@ void Compute_Den(fft_real *K, fft_real *P, int Nk, fft_real R_max, fft_real *del
 			if(j<box.nd[1]/2) ky = j*box.kl[1];
 			else ky = (j-box.nd[1])*box.kl[1];
 	
-            #pragma omp parallel for private(k, ind, ind2, kmod, std) 
+            //#pragma omp parallel for private(k, ind, ind2, kmod, std) 
 			for(k=0;k<box.nz2;k++){
 				kz = k*box.kl[2];
 				if(k == box.nd[2]/2)	kz = -box.nd[2]/2.0*box.kl[2];
 
                 ind = (size_t)(i*box.nd[1] + j)*((size_t)box.nz2) + (size_t)k;
-                
-                if(k == 0 && j == 0 && i > box.nd[0]/2){
-                    ind2 = (size_t)(box.nd[0] - i)*box.nd[1]*((size_t)box.nz2);
 
-                    deltak[ind][0] = deltak[ind2][0];
-                    deltak[ind][1] = deltak[ind2][1];
-                    deltak_tmp[ind][0] = deltak[ind][0];
-                    deltak_tmp[ind][1] = deltak[ind][1];
+                if((k == 0 || k == box.nd[2]/2) && (i > box.nd[0]/2 || j > box.nd[1]/2)){
+                    if(i > box.nd[0]/2 && j > box.nd[1]/2){
+                        ind2 = (size_t)((box.nd[0] - i)*box.nd[1] + (box.nd[1] - j))*((size_t)box.nz2) + k;
 
-                    continue;
-                }
+                        deltak[ind][0] = deltak[ind2][0];
+                        deltak[ind][1] = -deltak[ind2][1];
+                        deltak_tmp[ind][0] = deltak[ind][0];
+                        deltak_tmp[ind][1] = deltak[ind][1];
 
-                if(k == 0 && j > box.nd[1]/2){
-                    ind2 = (size_t)(i*box.nd[1] + (box.nd[1] - j))*((size_t)box.nz2);
+                        continue;
+                    }
+                    else if(i == 0 || i == box.nd[0]/2){
+                        ind2 = (size_t)(i*box.nd[1] + (box.nd[1] - j))*((size_t)box.nz2) + k;
 
-                    deltak[ind][0] = deltak[ind2][0];
-                    deltak[ind][1] = deltak[ind2][1];
-                    deltak_tmp[ind][0] = deltak[ind][0];
-                    deltak_tmp[ind][1] = deltak[ind][1];
+                        deltak[ind][0] = deltak[ind2][0];
+                        deltak[ind][1] = -deltak[ind2][1];
+                        deltak_tmp[ind][0] = deltak[ind][0];
+                        deltak_tmp[ind][1] = deltak[ind][1];
 
-                    continue;
+                        continue;                        
+                    }
+                    else if(j == 0 || j == box.nd[1]/2){
+                        ind2 = (size_t)((box.nd[0] - i)*box.nd[1] + j)*((size_t)box.nz2) + k;
+
+                        deltak[ind][0] = deltak[ind2][0];
+                        deltak[ind][1] = -deltak[ind2][1];
+                        deltak_tmp[ind][0] = deltak[ind][0];
+                        deltak_tmp[ind][1] = deltak[ind][1];
+
+                        continue;                         
+                    }
+                    else if(i > box.nd[0]/2 && j < box.nd[1]/2){
+                        ind2 = (size_t)((box.nd[0] - i)*box.nd[1] + (box.nd[1] - j))*((size_t)box.nz2) + k;
+
+                        deltak[ind][0] = deltak[ind2][0];
+                        deltak[ind][1] = -deltak[ind2][1];
+                        deltak_tmp[ind][0] = deltak[ind][0];
+                        deltak_tmp[ind][1] = deltak[ind][1];
+
+                        continue;                        
+                    }
                 }
 	
 				kmod = (fft_real) sqrt(kx*kx + ky*ky + kz*kz);	
 
-                if((kmod == 0.0 && R_max < 100000.0) || kmod <= k_smooth){
-                    if(kmod == 0.0)
+                if(kmod <= k_smooth){
+                    if(kmod > 0.0){
+                        std = (fft_real) sqrt(gsl_spline_eval(spline, (double) kmod, acc));
+                        if(fixed == FALSE)
+                            A = (fft_real) gsl_ran_gaussian(rng_ptr, (double) std);
+                        else
+                            A = std;                
+                    }
+                    else if(kmod == 0.0 && R_max < 100000.0){
                         kmod = (fft_real) pow(box.kl[0]*box.kl[1]*box.kl[2], 1.0/3.0)/4.0;
-                    std = (fft_real) sqrt(gsl_spline_eval(spline, (double) kmod, acc));
-                    if(fixed == FALSE)
-                        A = (fft_real) gsl_ran_gaussian(rng_ptr, (double) std);
+                        std = (fft_real) sqrt(gsl_spline_eval(spline, (double) kmod, acc));
+                        if(fixed == FALSE)
+                            A = (fft_real) gsl_ran_gaussian(rng_ptr, (double) std);
+                        else
+                            A = std;
+                    }
                     else
-                        A = std*(1.0 + 1e-6*(-1.0 + 2.0*(fft_real) gsl_rng_uniform(rng_ptr)));
+                        A = 0.0;
                 }
                 else
                     A = 0.0;
-                theta = 2.0*M_PI*((fft_real) gsl_rng_uniform(rng_ptr));
 	
 				/*Generate Gaussian random number with std*/
-                deltak[ind][0] = A*cos(theta + phase); 
-                deltak[ind][1] = A*sin(theta + phase);   
+                if((i == 0 || i == box.nd[0]/2) && (j == 0 || j == box.nd[1]/2) && (k == 0 || k == box.nd[2]/2)){
+                    deltak[ind][0] = A; 
+                    deltak[ind][1] = 0.0;
+                }
+                else{
+                    theta = 2.0*M_PI*((fft_real) gsl_rng_uniform(rng_ptr));
+                    deltak[ind][0] = A*cos(theta + phase); 
+                    deltak[ind][1] = A*sin(theta + phase);   
+                }
 				deltak_tmp[ind][0] = deltak[ind][0];
-				deltak_tmp[ind][1] = deltak[ind][1];
+				deltak_tmp[ind][1] = deltak[ind][1]; 
 	
 				if(isnan(deltak_tmp[ind][0]))	printf("Problem with deltak_tmp[%ld][0]\n", ind);
 				if(isnan(deltak_tmp[ind][1]))	printf("Problem with deltak_tmp[%ld][1]\n", ind);
@@ -227,4 +264,244 @@ void Compute_MS(fft_real *delta){
     std = std/((double)box.nd[0]*(double)box.nd[1]*(double)box.nd[2]);
 
     printf("Mean = %lf and Sigma = %lf\n", mean, sqrt(std - mean*mean));
+}
+
+/*Compute the density field to a given power*/
+void Compute_Den_to_n(fft_real *delta, fft_real *delta_n, int n){
+    size_t i;
+
+    for(i=0;i<box.ng;i++)
+        delta_n[i] = pow(delta[i], n);
+}
+
+/*Compute the potential field of a given density field*/
+void Compute_Phi(fft_real *delta, fft_complex *deltak, fft_real *phi){
+    int i, j, k, ink;
+    size_t ind;
+    fft_real kx, ky, kz, kmod, fact;
+    FFTW(plan) p1;
+
+    /*Create and compute deltak if it was not given*/
+    if(deltak == NULL){
+        fft_complex *deltak;
+        ink = FALSE;
+
+        /*Alloc the array for the density field in Fourier space*/
+        deltak = (fft_complex *) FFTW(malloc)((size_t)box.nd[0]*(size_t)box.nd[1]*(size_t)box.nz2*sizeof(fft_complex));
+        check_memory(deltak, "deltak")
+
+        /*Compute the density grid in Fourier space*/
+        Compute_Denk(delta, deltak);
+    }
+    else
+        ink = TRUE;
+
+	/*Compute the first order potential in Fourier space*/
+	for(i=0;i<box.nd[0];i++){
+		if(2*i<box.nd[0]) kx = i*box.kl[0];
+		else kx = (i-box.nd[0])*box.kl[0];
+	
+		for(j=0;j<box.nd[1];j++){
+			if(2*j<box.nd[1]) ky = j*box.kl[1];
+			else ky = (j-box.nd[1])*box.kl[1];
+	
+			for(k=0;k<box.nz2;k++){
+				kz = k*box.kl[2];
+				if(k == box.nd[2]/2) kz = -(fft_real)box.nd[2]/2.0*box.kl[2];
+
+				kmod = sqrt(pow(kx, 2.0) + pow(ky, 2.0) + pow(kz, 2.0));
+
+				ind = (size_t)(i*box.nd[1] + j)*(size_t)box.nz2 + (size_t)k;
+				if(kmod != 0.0){
+					fact = -pow(kmod, 2.0);
+					
+					deltak[ind][0] = deltak[ind][0]/fact;
+					deltak[ind][1] = deltak[ind][1]/fact;
+				}
+				else{
+					deltak[ind][0] = 0.0;
+					deltak[ind][1] = 0.0;
+				}
+			}
+		}
+	}
+
+	/*Compute the potential in real space*/
+    p1 = FFTW(plan_dft_c2r_3d)(box.nd[0], box.nd[1], box.nd[2], deltak, phi, FFTW_ESTIMATE); 
+	FFTW(execute)(p1);
+    FFTW(destroy_plan)(p1);
+    if(ink == FALSE)
+        FFTW(free)(deltak);
+
+    /*Normalize phi*/
+    #pragma omp parallel for private(ind) 
+    for(ind=0;ind<box.ng;ind++)
+        phi[ind] = box.Normx*phi[ind];
+}
+
+/*Compute the tidal field*/
+void Compute_Tidal(fft_real *delta, fft_complex *deltak, fft_real *tidal){
+    int i, j, k, ink, ki, kj, count;
+    size_t ind;
+    fft_real kmod, kvec[3], fact;
+    FFTW(plan) p1;
+    fft_complex *deltak_tmp;
+
+    /*Create and compute deltak if it was not given*/
+    if(deltak == NULL){
+        ink = FALSE;
+
+        /*Alloc the array for the density field in Fourier space*/
+        deltak = (fft_complex *) FFTW(malloc)((size_t)box.nd[0]*(size_t)box.nd[1]*(size_t)box.nz2*sizeof(fft_complex));
+        check_memory(deltak, "deltak")
+
+        /*Compute the density grid in Fourier space*/
+        Compute_Denk(delta, deltak);
+
+    }
+    else
+        ink = TRUE;
+
+    /*Alloc the temporary array*/
+    deltak_tmp = (fft_complex *) FFTW(malloc)((size_t)box.nd[0]*(size_t)box.nd[1]*(size_t)box.nz2*sizeof(fft_complex));
+    check_memory(deltak_tmp, "deltak_tmp")
+
+	/*Compute each component of the tidal field*/
+    count = 0;
+    for(ki=0;ki<3;ki++){
+        for(kj=ki;kj<3;kj++){
+            for(i=0;i<box.nd[0];i++){
+                if(2*i<box.nd[0]) kvec[0] = i*box.kl[0];
+                else kvec[0] = (i-box.nd[0])*box.kl[0];
+            
+                for(j=0;j<box.nd[1];j++){
+                    if(2*j<box.nd[1]) kvec[1] = j*box.kl[1];
+                    else kvec[1] = (j-box.nd[1])*box.kl[1];
+            
+                    for(k=0;k<box.nz2;k++){
+                        kvec[2] = k*box.kl[2];
+                        if(k == box.nd[2]/2) kvec[2] = -(fft_real)box.nd[2]/2.0*box.kl[2];
+
+                        kmod = sqrt(pow(kvec[0], 2.0) + pow(kvec[1], 2.0) + pow(kvec[2], 2.0));
+                        ind = (size_t)(i*box.nd[1] + j)*(size_t)box.nz2 + (size_t)k;
+
+                        if(kmod != 0.0){
+                            fact =  kvec[ki]*kvec[kj]/pow(kmod, 2.0);
+                            
+                            deltak_tmp[ind][0] = fact*deltak[ind][0];
+                            deltak_tmp[ind][1] = fact*deltak[ind][1];
+                        }
+                        else{
+                            deltak_tmp[ind][0] = 0.0;
+                            deltak_tmp[ind][1] = 0.0;
+                        }
+                    }
+                }
+            }
+
+            /*Compute the potential in real space*/
+            p1 = FFTW(plan_dft_c2r_3d)(box.nd[0], box.nd[1], box.nd[2], deltak_tmp, &tidal[count*box.ng], FFTW_ESTIMATE); 
+            FFTW(execute)(p1);
+
+            /*Normalize phi*/
+            #pragma omp parallel for private(ind) 
+            for(ind=0;ind<box.ng;ind++)
+                tidal[count*box.ng + ind] = box.Normx*tidal[count*box.ng + ind];
+
+            count ++;
+        }
+    }
+
+    /*Free the memory*/
+    FFTW(destroy_plan)(p1);
+    FFTW(free)(deltak_tmp);
+    if(ink == FALSE)
+        FFTW(free)(deltak);
+}
+
+/*Get the position in the flat array of the tidal field*/
+int Get_IndK(int i, int j){
+    int resp;
+
+    if(j < i){
+        resp = j;
+        j = i;
+        i = resp;
+    }
+
+    if(i == 0)
+        resp = j;
+    else if(i == 1)
+        resp = 2 + j;
+    else if(i == 2)
+        resp = 5;
+
+    return resp;
+}
+
+/*Compute K2 given the density field or the tidal field and the subtraction of the delta field (given by a):K2 =  K^2 - a*delta^2*/
+void Compute_K2(fft_real *delta, fft_complex *deltak, fft_real *tidal, fft_real *K2, fft_real a){
+    size_t ind;
+    int intd;
+    fft_real trace, K2_tmp;
+
+    /*Create and compute the tidal field if it was not given*/
+    if(tidal == NULL){
+        intd = FALSE;
+
+        /*Alloc the array for the tidal field*/
+        tidal = (fft_real *) malloc(6*(size_t)box.nd[0]*(size_t)box.nd[1]*(size_t)box.nz2*sizeof(fft_real));
+        check_memory(tidal, "tidal")
+
+        /*Compute the density grid in Fourier space*/
+        Compute_Tidal(delta, deltak, tidal);
+    }
+    else
+        intd = TRUE;
+
+    /*Compute K2*/
+	for(ind=0;ind<box.ng;ind++){
+        trace = tidal[0*box.ng + ind] + tidal[3*box.ng + ind] + tidal[5*box.ng + ind];
+        K2_tmp = pow(tidal[0*box.ng + ind], 2.0) + pow(tidal[3*box.ng + ind], 2.0) + pow(tidal[5*box.ng + ind], 2.0) + 2.0*(pow(tidal[1*box.ng + ind], 2.0) + pow(tidal[2*box.ng + ind], 2.0) + pow(tidal[4*box.ng + ind], 2.0));
+
+        K2[ind] = K2_tmp - a*pow(trace, 2.0);
+    }
+
+    /*Free the array with the tidal field if it was not give*/
+    if(intd == FALSE)
+        free(tidal);
+}
+
+/*Compute K3 given the density field or the tidal field*/
+void Compute_K3(fft_real *delta, fft_complex *deltak, fft_real *tidal, fft_real *K3, fft_real a, fft_real b){
+    size_t ind;
+    int intd, i, j, k;
+    fft_real trace, K2, K3_tmp;
+
+    /*Create and compute the tidal field if it was not given*/
+    if(tidal == NULL){
+        intd = FALSE;
+
+        /*Alloc the array for the tidal field*/
+        tidal = (fft_real *) malloc(6*(size_t)box.nd[0]*(size_t)box.nd[1]*(size_t)box.nz2*sizeof(fft_real));
+        check_memory(tidal, "tidal")
+
+        /*Compute the density grid in Fourier space*/
+        Compute_Tidal(delta, deltak, tidal);
+    }
+    else
+        intd = TRUE;
+
+    /*Compute K2*/
+	for(ind=0;ind<box.ng;ind++){
+        trace = tidal[0*box.ng + ind] + tidal[3*box.ng + ind] + tidal[5*box.ng + ind];
+        K2 = pow(tidal[0*box.ng + ind], 2.0) + pow(tidal[3*box.ng + ind], 2.0) + pow(tidal[5*box.ng + ind], 2.0) + 2.0*(pow(tidal[1*box.ng + ind], 2.0) + pow(tidal[2*box.ng + ind], 2.0) + pow(tidal[4*box.ng + ind], 2.0));
+        K3_tmp = pow(tidal[0*box.ng + ind], 3.0) + pow(tidal[3*box.ng + ind], 3.0) + pow(tidal[5*box.ng + ind], 3.0) + 3.0*(pow(tidal[1*box.ng + ind], 2.0)*(tidal[0*box.ng + ind] + tidal[3*box.ng + ind]) + pow(tidal[2*box.ng + ind], 2.0)*(tidal[0*box.ng + ind] + tidal[5*box.ng + ind]) + pow(tidal[4*box.ng + ind], 2.0)*(tidal[3*box.ng + ind] + tidal[5*box.ng + ind])) + 6.0*tidal[1*box.ng + ind]*tidal[2*box.ng + ind]*tidal[4*box.ng + ind];
+            
+        K3[ind] = K3_tmp - a*K2*trace + b*pow(trace, 3.0);
+    }
+
+    /*Free the array with the tidal field if it was not give*/
+    if(intd == FALSE)
+        free(tidal);
 }
--- exshalos/exshalos/density_grid.h
+++ exshalos/exshalos/density_grid.h
@@ -21,4 +21,22 @@ void Save_Den(char *denfile, fft_real *delta);
 /*Compute the mean and std of the density field*/
 void Compute_MS(fft_real *delta);
 
+/*Compute the density field to a given power*/
+void Compute_Den_to_n(fft_real *delta, fft_real *delta_n, int n);
+
+/*Compute the potential field of a given density field*/
+void Compute_Phi(fft_real *delta, fft_complex *deltak, fft_real *phi);
+
+/*Compute the tidal field*/
+void Compute_Tidal(fft_real *delta, fft_complex *deltak, fft_real *tidal);
+
+/*Get the position in the flat array of the tidal field*/
+int Get_IndK(int i, int j);
+
+/*Compute K2 given the density field or the tidal field and the subtraction of the delta field (given by a):K2 =  K^2 - a*delta^2*/
+void Compute_K2(fft_real *delta, fft_complex *deltak, fft_real *tidal, fft_real *K2, fft_real a);
+
+/*Compute K3 given the density field or the tidal field*/
+void Compute_K3(fft_real *delta, fft_complex *deltak, fft_real *tidal, fft_real *K3, fft_real a, fft_real b);
+
 #endif
