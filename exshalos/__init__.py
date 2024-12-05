cb21897efe3d30360bccc1122ea17aafa049768d
Voivodic <rodrigo.voivodic@gmail.com>
2024-12-05 14:09:49 +0100

    Updated main __init__.py removing no configured modules

--- exshalos/__init__.py
+++ exshalos/__init__.py
@@ -1,11 +1,11 @@
-#C modules
+# C modules
 from .spectrum.spectrum import *
 from .exshalos.exshalos import *
 from .hod.hod import *
-from .sampler.sampler import *
-from .analytical.analytical import *
+# from .sampler.sampler import *
+# from .analytical.analytical import *
 
-#python modules
+# python modules
 from .theory import *
 from .simulation import *
 from .utils import *
